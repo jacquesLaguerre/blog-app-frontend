a2adebc03049ee58a695bff18516bf67d8f6d679
Jacques Laguerre <jacquesblaguerre@gmail.com>
2022-11-23 09:57:48 -0500

    complete-fe

---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return ( 
+   <footer><small>Copyrights &copy; 2022 | all rights reserved</small></footer>
+  )
+}
---
+++ src/pages/AddPost.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+export default function AddPost() {
+  const [form, setForm] = useState({})
+
+  const submitPost = e => {
+    e.preventDefault()
+
+    fetch('http://localhost:3030', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    })
+  }
+
+  const handleForm = e => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  console.log(form)
+
+  return (
+    <div className='container'>
+      <h1>Add Post</h1>
+      <form action='submit'>
+        <label htmlFor=''>Author</label>
+        <input name='author' type='text' onChange={handleForm} />
+        <br />
+        <label htmlFor=''>Text</label>
+        <input name='text' type='text' onChange={handleForm} />
+        <br />
+        <label htmlFor=''>Date</label>
+        <input name='date' type='date' onChange={handleForm} />
+        <br />
+        <button type='submit' onClick={submitPost}>
+          Add new post
+        </button>
+      </form>
+    </div>
+  )
+}
